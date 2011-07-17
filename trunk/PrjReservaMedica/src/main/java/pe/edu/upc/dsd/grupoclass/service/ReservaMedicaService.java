3a8743d571ee882dfc9ef0ee958f53d75df4434b
cjulcapoma@gmail.com <cjulcapoma@gmail.com@4c451aa2-4a6e-159e-afad-0c8b989dc099>
2011-07-17 23:48:05 +0000

    Revisión de los métodos verificarDisponibilidadDoctor() y actualizarDisponibilidadDoctor()

--- trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/service/ReservaMedicaService.java
+++ trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/service/ReservaMedicaService.java
@@ -3,6 +3,7 @@ package pe.edu.upc.dsd.grupoclass.service;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import pe.edu.upc.dsd.grupoclass.bean.DoctorBean;
@@ -10,11 +11,16 @@ import pe.edu.upc.dsd.grupoclass.bean.HorarioDoctorBean;
 import pe.edu.upc.dsd.grupoclass.bean.ReservaMedicaBean;
 
 @WebService
-public interface ReservaMedicaService {	
-	
+public interface ReservaMedicaService {
+
 	public boolean registrarReservaMedica(ReservaMedicaBean reservaMedicaBean);
-	public boolean verificarDisponibilidadDoctor(int idDoctor, Date fechaHora);
+
+	public boolean verificarDisponibilidadDoctor(
+			@WebParam(name = "idDoctor") int idDoctor,
+			@WebParam(name = "fechaHora") Date fechaHora);
+
 	public boolean actualizarDisponibilidadDoctor(int idDoctor, Date fechaHora);
+
 	public ReservaMedicaBean obtenerReservaPaciente(String dni);
-	
+
 }
--- trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/service/impl/ReservaMedicaServiceImpl.java
+++ trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/service/impl/ReservaMedicaServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import pe.edu.upc.dsd.grupoclass.bean.DoctorBean;
@@ -11,24 +12,24 @@ import pe.edu.upc.dsd.grupoclass.bean.HorarioDoctorBean;
 import pe.edu.upc.dsd.grupoclass.bean.ReservaMedicaBean;
 import pe.edu.upc.dsd.grupoclass.data.DataClass;
 import pe.edu.upc.dsd.grupoclass.service.ReservaMedicaService;
-	
+
 @WebService(endpointInterface = "pe.edu.upc.dsd.grupoclass.service.ReservaMedicaService")
 public class ReservaMedicaServiceImpl implements ReservaMedicaService {
-	
+
 	public ArrayList<String> listaSucursales;
 	public ArrayList<String> listaEspecialidades;
-	public ArrayList<DoctorBean> listaDoctores;	
+	public ArrayList<DoctorBean> listaDoctores;
 	public ArrayList<HorarioDoctorBean> listaHorariosDoctor;
-	
+
 	public ArrayList<ReservaMedicaBean> listaReservasMedicas = new ArrayList();
-	
-	public ReservaMedicaServiceImpl(){	
+
+	public ReservaMedicaServiceImpl() {
 		listaSucursales = DataClass.listarSucursales();
 		listaEspecialidades = DataClass.listarEspecialidades();
 		listaDoctores = DataClass.listarDoctores();
 		listaHorariosDoctor = DataClass.listarHorariosDoctor();
 	}
-	
+
 	public boolean registrarReservaMedica(ReservaMedicaBean reservaMedicaBean) {
 		// TODO Auto-generated method stub
 		return false;
@@ -36,11 +37,33 @@ public class ReservaMedicaServiceImpl implements ReservaMedicaService {
 
 	public boolean verificarDisponibilidadDoctor(int idDoctor, Date fechaHora) {
 		// TODO Auto-generated method stub
-		return false;
+		// Revisar horario Doctor
+		for (HorarioDoctorBean horario : listaHorariosDoctor) {
+			if (horario.getIdDoctor() == idDoctor
+					&& horario.getFechaHora().equals(fechaHora)) {
+				return false;
+			}
+		}
+		// Revisar reservas realizadas
+		for (ReservaMedicaBean reserva : listaReservasMedicas) {
+			if (reserva.getDoctor().getIdDoctor() == idDoctor
+					&& reserva.getFechaHoraReserva().equals(fechaHora)
+					&& reserva.isIndReserva()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public boolean actualizarDisponibilidadDoctor(int idDoctor, Date fechaHora) {
 		// TODO Auto-generated method stub
+		for (ReservaMedicaBean reserva : listaReservasMedicas) {
+			if (reserva.getDoctor().getIdDoctor()== idDoctor
+					&& reserva.getFechaHoraReserva().equals(fechaHora)) {
+				reserva.setIdReservaMedica(1);
+				return true;
+			}
+		}
 		return false;
 	}
 
