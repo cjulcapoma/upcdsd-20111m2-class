9fb61987a73d4cbd0f6f1ee369fb952d6529175d
cjulcapoma@gmail.com <cjulcapoma@gmail.com@4c451aa2-4a6e-159e-afad-0c8b989dc099>
2011-07-18 05:09:55 +0000

    Revisión DataClass

--- trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/data/DataClass.java
+++ trunk/PrjReservaMedica/src/main/java/pe/edu/upc/dsd/grupoclass/data/DataClass.java
@@ -1,15 +1,18 @@
 package pe.edu.upc.dsd.grupoclass.data;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import pe.edu.upc.dsd.grupoclass.bean.DoctorBean;
 import pe.edu.upc.dsd.grupoclass.bean.HorarioDoctorBean;
-import pe.edu.upc.dsd.grupoclass.bean.ReservaMedicaBean;
 
 public class DataClass {
 	
 	public static ArrayList<String> listarSucursales(){
 		ArrayList<String> listaSucursales = new ArrayList();
+		
 		listaSucursales.add("Sucursal San Borja");
 		
 		return listaSucursales;
@@ -17,19 +20,52 @@ public class DataClass {
 	
 	public static ArrayList<String> listarEspecialidades(){
 		ArrayList<String> listaEspecialidades = new ArrayList();
-		listaEspecialidades.add("Medicina General");
+		
+		listaEspecialidades.add("Medicina General");		
+		listaEspecialidades.add("Oftamología");
+		listaEspecialidades.add("Traumatología");
 		
 		return listaEspecialidades;
 	}
 	
 	public static ArrayList<DoctorBean> listarDoctores(){
 		ArrayList<DoctorBean> listaDoctores = new ArrayList();
+		DoctorBean doctor1 = new DoctorBean();
 		
+		doctor1.setIdDoctor(1);
+		doctor1.setNoDoctor("Rafael Juan");
+		doctor1.setNoEspecialidad("Medicina General");
+		doctor1.setNoSucursal("Sucursal San Borja");
+		
+		DoctorBean doctor2 = new DoctorBean();
+		
+		doctor2.setIdDoctor(2);
+		doctor2.setNoDoctor("Juan Luis");
+		doctor2.setNoEspecialidad("Oftamología");
+		doctor2.setNoSucursal("Sucursal San Borja");
+		listaDoctores.add(doctor2);
+
+		DoctorBean doctor3 = new DoctorBean();
+		
+		doctor3.setIdDoctor(3);
+		doctor3.setNoDoctor("Juan Carlos");
+		doctor3.setNoEspecialidad("Traumatología");
+		doctor3.setNoSucursal("Sucursal San Borja");
+		listaDoctores.add(doctor3);
+
 		return listaDoctores;
 	}
 	
 	public static ArrayList<HorarioDoctorBean> listarHorariosDoctor(){
 		ArrayList<HorarioDoctorBean> listaHorariosDoctor = new ArrayList();
+		HorarioDoctorBean hdoctor1_h1 = new HorarioDoctorBean();
+		Date fecha1 = new Date();
+		Calendar cal = GregorianCalendar.getInstance();
+		cal.set(2011,07,17,8,0,0);
+		fecha1 = cal.getTime();
+		hdoctor1_h1.setIdDoctor(1);
+		hdoctor1_h1.setFechaHora(fecha1);		
+		listaHorariosDoctor.add(hdoctor1_h1);
 		
 		return listaHorariosDoctor;
 	}
