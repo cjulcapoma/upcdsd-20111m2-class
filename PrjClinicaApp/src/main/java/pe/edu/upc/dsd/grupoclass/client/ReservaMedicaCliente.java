16b7c21fefd6bbcaa146d6c5c43ad6b555a69534
cjulcapoma <cjulcapoma@4c451aa2-4a6e-159e-afad-0c8b989dc099>
2011-08-12 13:04:08 +0000

    JSP/Servlet Reserva Medica

--- PrjClinicaApp/src/main/java/pe/edu/upc/dsd/grupoclass/client/ReservaMedicaCliente.java
+++ PrjClinicaApp/src/main/java/pe/edu/upc/dsd/grupoclass/client/ReservaMedicaCliente.java
@@ -4,7 +4,9 @@ import java.rmi.RemoteException;
 
 import javax.xml.rpc.ServiceException;
 
+import pe.edu.upc.dsd.grupoclass.bean.ConsultaMedicaBean;
 import pe.edu.upc.dsd.grupoclass.bean.ReservaMedicaBean;
+import pe.edu.upc.dsd.grupoclass.service.ConsultaMedicaService;
 import pe.edu.upc.dsd.grupoclass.service.ReservaMedicaService;
 import pe.edu.upc.dsd.grupoclass.service.impl.ReservaMedicaServiceImplServiceLocator;
 
@@ -36,5 +38,46 @@ public class ReservaMedicaCliente {
 		
 		return reservaMedicaBean2;
 	}
-
+	public ReservaMedicaBean[] obtenerReservasMedicas() throws RemoteException{
+		ReservaMedicaServiceImplServiceLocator reservaMedicaServiceLocator =
+			new ReservaMedicaServiceImplServiceLocator();
+		ReservaMedicaBean reservaMedicaBean2 = new ReservaMedicaBean();
+		try {
+			ReservaMedicaService reservaMedicaWS =
+				reservaMedicaServiceLocator.getReservaMedicaServiceImplPort();
+			
+			ReservaMedicaBean reservaMedicaBean = new ReservaMedicaBean();
+			reservaMedicaBean.setNoPaciente("Luis Kina");
+			reservaMedicaBean.setDniPaciente("45477388");
+			reservaMedicaBean.setIndReserva(true);
+			
+			reservaMedicaWS.registrarReservaMedica(reservaMedicaBean);
+						
+			return reservaMedicaWS.obtenerReservasMedicas();
+			
+		} catch (ServiceException e) {
+			System.out.println("errorrr");
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	public boolean registrarResultadoReservaMedica(ReservaMedicaBean reservaMedicaBean) throws RemoteException{		
+		boolean resultado = false;
+		ReservaMedicaServiceImplServiceLocator reservaMedicaServiceLocator =
+			new ReservaMedicaServiceImplServiceLocator();
+		try {
+			ReservaMedicaService reservaMedicaWS =
+				reservaMedicaServiceLocator.getReservaMedicaServiceImplPort();
+						
+			resultado = reservaMedicaWS.registrarReservaMedica(reservaMedicaBean);
+			
+			System.out.println("listaReserva: "+reservaMedicaWS.obtenerReservasMedicas().length);
+			
+		} catch (ServiceException e) {
+			System.out.println("errorrr");
+			e.printStackTrace();
+		}		
+		return resultado;
+	}
 }
