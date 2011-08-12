04a65e26fc8491cbb28305f2e09fad158558c25f
jlkina@gmail.com <jlkina@gmail.com@4c451aa2-4a6e-159e-afad-0c8b989dc099>
2011-08-12 07:44:55 +0000

    Funciona el rest / soap / colas

--- PrjClinicaApp/src/main/java/pe/edu/upc/dsd/grupoclass/controller/VentaMedicamentosServlet.java
+++ PrjClinicaApp/src/main/java/pe/edu/upc/dsd/grupoclass/controller/VentaMedicamentosServlet.java
@@ -23,7 +23,6 @@ import pe.edu.upc.dsd.grupoclass.bean.MedicamentoRecetaBean;
 import pe.edu.upc.dsd.grupoclass.bean.ReciboCobroBean;
 import pe.edu.upc.dsd.grupoclass.client.ConsultaMedicaCliente;
 import pe.edu.upc.dsd.grupoclass.dao.ConstantesDao;
-import pe.edu.upc.dsd.grupoclass.jms.MessageProducer;
 
 import com.google.gson.Gson;
 
@@ -50,63 +49,176 @@ public class VentaMedicamentosServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("en el do gettt");
 		System.out.println("antess del metodoo");
-		
 
-
-		MessageProducer messageProducer = new MessageProducer();
-
+		//System.out.println("despues de carga");
 
 		final RestTemplate restTemplate = new RestTemplate();
 		Gson gson = new Gson();
 		List<MedicamentoRecetaBean> medicamentos = new ArrayList<MedicamentoRecetaBean>();
 		HttpSession session = request.getSession();
-		
-				
+
 		ConsultaMedicaBean consultaMedicaBean = null;
 		ConsultaMedicaCliente consultaMedicaCliente = new ConsultaMedicaCliente();
 		ConstantesDao constantesDao = new ConstantesDao();
 		Double monto = 0.00;
 
-		session.setAttribute("afililiado", "No");
-		session.setAttribute("porcentajeDescuento", "N/A");
-		session.setAttribute("descuento", "N/A");
-		session.setAttribute("monto", "N/A");
-		session.setAttribute("montoT", "N/A");
-		session.setAttribute("hdDni", "0");
-		session.setAttribute("hdPaciente", "0");
-		session.setAttribute("hdMontoT", "0");
-		session.setAttribute("hdDescuento", "0");
+		// session.setAttribute("medicinas","");
 		System.out.println(session.getAttribute("hdMontoT"));
 		/*
-		String dniC = request.getParameter("hdDni");
-		String pacienteC = request.getParameter("hdPaciente");
-		String montoPC = request.getParameter("hdMontoT");
-		String descuentoC = request.getParameter("hdDescuento");
-		*/
-		
-		
+		 * String dniC = request.getParameter("hdDni"); String pacienteC =
+		 * request.getParameter("hdPaciente"); String montoPC =
+		 * request.getParameter("hdMontoT"); String descuentoC =
+		 * request.getParameter("hdDescuento");
+		 */
+
 		String accion = request.getParameter("hdAccion").toString();
+
+		if (accion.equals("carga")) {
+			// carga de datos ficticia
+
+			MedicamentoRecetaBean medicamentoRecetaBean = new MedicamentoRecetaBean();
+			ArrayList<MedicamentoRecetaBean> listaMedicamentostt = new ArrayList<MedicamentoRecetaBean>();
+
+			// for(int i=0; i<listaMedicamentostt.length; i++){
+			// listaMedicamentos9[i].add(listaMedicamentos[i]);
+
+			medicamentoRecetaBean.setCantidad(1);
+			medicamentoRecetaBean.setCoMedicamento("2");
+			medicamentoRecetaBean.setPrecio(100);
+			medicamentoRecetaBean.setNoMedicamento("veneno");
+			medicamentoRecetaBean.setIdMedicamento(5);
+			// listaMedicamentostt.add(medicamentoRecetaBean);
+
+			System.out.println("antes de []");
+			MedicamentoRecetaBean[] listaMedicamentos9 = new MedicamentoRecetaBean[] { medicamentoRecetaBean };
+
+			//listaMedicamentos9[0].setCantidad(1);
+			//listaMedicamentos9[0].setCoMedicamento("2");
+			//listaMedicamentos9[0].setPrecio(100);
+			//listaMedicamentos9[0].setNoMedicamento("veneno");
+			//listaMedicamentos9[0].setIdMedicamento(5);
+			// listaMedicamentostt.add(medicamentoRecetaBean);
+			// listaMedicamentostt.add(listaMedicamentos9[0]);
+
+			ConsultaMedicaBean consultaMedicaBeantt = new ConsultaMedicaBean();
+
+			consultaMedicaBeantt.setListaMedicamentos(listaMedicamentos9);
+			consultaMedicaBeantt.setNoPaciente("Luis Kina");
+			consultaMedicaBeantt.setDniPaciente("12345678");
+			System.out.println("222222222222");
+			/*
+			 * consultaMedicaBean.setListaMedicamentos(listaMedicamentos);
+			 * consultaMedicaBean.setNoPaciente("Luis Kina");
+			 */
+			consultaMedicaCliente.registrarConsultaMedica(consultaMedicaBeantt);
+
+			System.out.println("3333333333");
+
+			System.out.println("listaaaa::::"
+					+ consultaMedicaCliente.obtenerConsultaMedica(3)
+							.getNoPaciente());
+			System.out.println("listaaaa::::"
+					+ consultaMedicaCliente.obtenerConsultaMedica(3)
+							.getListaMedicamentos(0).getNoMedicamento());
+
+		}
 
 		if (accion.equals("obtenerDatosPaciente")) {
 			String nroConsulta = request.getParameter("nroConsulta");
-			if (nroConsulta != null) {
-				if (nroConsulta != "") {
+			if (nroConsulta == null){
+				
+				MedicamentoRecetaBean medicamentoRecetaBean3 = new MedicamentoRecetaBean();
+				// for(int i=0; i<listaMedicamentostt.length; i++){
+				// listaMedicamentos9[i].add(listaMedicamentos[i]);
+
+				medicamentoRecetaBean3.setCantidad(0);
+				medicamentoRecetaBean3.setCoMedicamento("");
+				medicamentoRecetaBean3.setPrecio(0);
+				medicamentoRecetaBean3.setNoMedicamento("");
+				medicamentoRecetaBean3.setIdMedicamento(0);
+				// listaMedicamentostt.add(medicamentoRecetaBean);
+
+				System.out.println("limpia med1");
+				MedicamentoRecetaBean[] listaMedicamentos10 = new MedicamentoRecetaBean[] { medicamentoRecetaBean3 };
+				session.setAttribute("medicinas", listaMedicamentos10);
+				
+				
+				
+				session.removeValue("consulta");
+				session.setAttribute("montoT", "0");;
+				session.setAttribute("afililiado", "No");
+				session.setAttribute("porcentajeDescuento","0");
+				session.setAttribute("consulta.noPaciente","");
+				session.setAttribute("consulta.dniPaciente","");
+				session.setAttribute("medicina", "");
+				//session.setAttribute("medicina.coMedicamento", "");
+				//session.setAttribute("medicina.noMedicamento", "");
+				//session.setAttribute("medicina.cantidad", "");
+				//session.setAttribute("medicina.precio", "");
+				session.setAttribute("afililiado", "No");
+				session.setAttribute("descuento", "N/A");
+				session.setAttribute("hdDni", "0");
+				session.setAttribute("hdPaciente", "0");
+				session.setAttribute("hdMontoT", "0");
+				session.setAttribute("vuelto","0");
+				//session.setAttribute("consulta","");
+				
+			} else{
+				if (nroConsulta == "") {
+					
+					MedicamentoRecetaBean medicamentoRecetaBean3 = new MedicamentoRecetaBean();
+					
+					medicamentoRecetaBean3.setCantidad(0);
+					medicamentoRecetaBean3.setCoMedicamento("");
+					medicamentoRecetaBean3.setPrecio(0);
+					medicamentoRecetaBean3.setNoMedicamento("");
+					medicamentoRecetaBean3.setIdMedicamento(0);
+					// listaMedicamentostt.add(medicamentoRecetaBean);
+
+					System.out.println("limpia med1");
+					MedicamentoRecetaBean[] listaMedicamentos10 = new MedicamentoRecetaBean[] { medicamentoRecetaBean3 };
+					session.setAttribute("medicinas", listaMedicamentos10);
+					
+
+					session.removeValue("consulta");
+					session.setAttribute("montoT", "0");;
+					session.setAttribute("afililiado", "No");
+					session.setAttribute("porcentajeDescuento","0");
+					//session.setAttribute("consulta","");
+					session.setAttribute("consulta.noPaciente","");
+					session.setAttribute("consulta.dniPaciente","");
+					//session.setAttribute("medicina", "");
+					//session.setAttribute("medicina.coMedicamento", "");
+					//session.setAttribute("medicina.noMedicamento", "");
+					//session.setAttribute("medicina.cantidad", "");
+					//session.setAttribute("medicina.precio", "");
+					session.setAttribute("afililiado", "No");
+					session.setAttribute("descuento", "N/A");
+					session.setAttribute("hdDni", "0");
+					session.setAttribute("hdPaciente", "0");
+					session.setAttribute("hdMontoT", "0");
+					session.setAttribute("vuelto","0");
+				}else
+				{
 					System.out.println(nroConsulta);
 					consultaMedicaBean = consultaMedicaCliente
 							.obtenerConsultaMedica(Integer
 									.parseInt(nroConsulta));
 
 					if (consultaMedicaBean != null) {
-						System.out.println(nroConsulta);
+						System.out.println(nroConsulta + "session");
 
 						session.setAttribute("nroConsulta", nroConsulta);
 						session.setAttribute("consulta", consultaMedicaBean);
 						MedicamentoRecetaBean[] listaMedicamentos = consultaMedicaBean
 								.getListaMedicamentos();
 
+						System.out.println("antes de lista med");
+						System.out.println(listaMedicamentos.length);
 						if (listaMedicamentos != null) {
 							session.setAttribute("medicinas", listaMedicamentos);
 
+							System.out.println(listaMedicamentos.length);
 							for (int i = 0; i < listaMedicamentos.length; i++) {
 								monto = monto
 										+ listaMedicamentos[i].getPrecio();
@@ -132,6 +244,16 @@ public class VentaMedicamentosServlet extends HttpServlet {
 
 							if (result.equalsIgnoreCase("no")) {
 								session.setAttribute("montoT", monto);
+								session.setAttribute("hdDni", "0");
+								session.setAttribute("hdPaciente", "0");
+								session.setAttribute("hdMontoT", "0");
+								session.setAttribute("hdDescuento", "0");
+								session.setAttribute("afililiado", "No");
+								session.setAttribute("porcentajeDescuento",
+										"N/A");
+								session.setAttribute("descuento", "N/A");
+								// session.setAttribute("monto", "N/A");
+								// session.setAttribute("montoT", "N/A");
 							} else {
 							}
 							AfiliadoBean afiliado = new AfiliadoBean();
@@ -146,6 +268,14 @@ public class VentaMedicamentosServlet extends HttpServlet {
 									afiliado.desMedicina);
 							session.setAttribute("descuento", mDescuento);
 							session.setAttribute("montoT", monto - mDescuento);
+							session.setAttribute("hdDni", afiliado.dni);
+							session.setAttribute("hdPaciente", afiliado.nombre);
+							session.setAttribute("hdMontoT", monto - mDescuento);
+							//session.setAttribute("hdDescuento", mDescuento);
+							System.out.println(afiliado.nombre);
+							System.out.println( monto - mDescuento);
+							System.out.println(session.getAttribute("hdDni"));
+							
 
 						}
 					}
@@ -154,56 +284,66 @@ public class VentaMedicamentosServlet extends HttpServlet {
 		}
 
 		if (accion.equals("enviarCola")) {
+
+			System.out.println(session.getAttribute("hdMontoT"));
+			System.out.println(session.getAttribute("hdPaciente"));
+			System.out.println(session.getAttribute("hdDni"));
+			System.out.println(session.getAttribute("efectivo"));
 			
 			
 			
-			/*
-			String dniC = request.getParameter("hdDni");
-			String pacienteC = request.getParameter("hdPaciente");
-			String montoPC = request.getParameter("hdMontoT");
-			String descuentoC = request.getParameter("hdDescuento");
 			
-*/
-			
+			String dniC =  session.getAttribute("hdDni").toString();
+			String pacienteC =  session.getAttribute("hdPaciente").toString();
+			String montoPC = session.getAttribute("hdMontoT").toString();
+			String efectivoC =  request.getParameter("efectivo");
+			// */
+
 			System.out.println("cola");
 			ReciboCobroBean reciboCobro = new ReciboCobroBean();
 
-			/*if (montoPC.equalsIgnoreCase("0")) {}else{
-				
-				reciboCobro.setDniPaciente(dniC);			
+			if (montoPC.equalsIgnoreCase("0")) {
+			} else {
+				System.out.println("cola double");
+				//System.out.println(dniC);
+				System.out.println(pacienteC);
+				System.out.println(montoPC);
+				System.out.println(efectivoC );
+				reciboCobro.setDniPaciente(dniC);
 				reciboCobro.setNoPaciente(pacienteC);
-				reciboCobro.setMntPagado(Double.valueOf(montoPC) - Double.valueOf(descuentoC));
-				 
-				  */
-			reciboCobro.setDniPaciente("43886126");			
-			reciboCobro.setNoPaciente("Juan Diaz");
-			reciboCobro.setMntPagado(200);
-			
+				reciboCobro.setMntPagado(Double.parseDouble(montoPC));
+				session.setAttribute("vuelto",Double.parseDouble(efectivoC) -Double.parseDouble(montoPC));
 
-			ByteArrayOutputStream bs = new ByteArrayOutputStream();
-			ObjectOutputStream os = new ObjectOutputStream(bs);
-			os.writeObject(reciboCobro); // this es de tipo DatoUdp
-			os.close();
-			byte[] bytes = bs.toByteArray(); // devuelve byte[]
+				// */
+				// reciboCobro.setDniPaciente("12312312");
+				// reciboCobro.setNoPaciente("Juan Diaz");
+				// reciboCobro.setMntPagado(666);
 
-			System.out.println(reciboCobro.getDniPaciente());
-			System.out.println("cola2");
-		
-			/*messageProducer.send("HHHH");*/
-	        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+				ByteArrayOutputStream bs = new ByteArrayOutputStream();
+				ObjectOutputStream os = new ObjectOutputStream(bs);
+				os.writeObject(reciboCobro); // this es de tipo DatoUdp
+				os.close();
+				byte[] bytes = bs.toByteArray(); // devuelve byte[]
 
+				System.out.println(reciboCobro.getDniPaciente());
+				System.out.println("cola2");
 
-			final JmsTemplate jmsTemplate;
-			jmsTemplate = new JmsTemplate();
-			jmsTemplate.setConnectionFactory(connectionFactory);
-			jmsTemplate.convertAndSend("colaVentas",bytes);
-			
-//}
+				/* messageProducer.send("HHHH"); */
+				ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+						"tcp://localhost:61616");
+
+				final JmsTemplate jmsTemplate;
+				jmsTemplate = new JmsTemplate();
+				jmsTemplate.setConnectionFactory(connectionFactory);
+				jmsTemplate.convertAndSend("colaVentas", bytes);
+
+			}
 			System.out.println("cola3");
 		}
 		System.out.println("pasooo");
 
 		request.getRequestDispatcher("frmVentaMedicamentos.jsp").forward(
 				request, response);
+
 	}
 }
--- PrjClinicaApp/src/main/webapp/frmVentaMedicamentos.jsp
+++ PrjClinicaApp/src/main/webapp/frmVentaMedicamentos.jsp
@@ -22,160 +22,181 @@
 		this.document.forms[0].action = 'ventaMedicamentosServlet';
 		this.document.forms[0].submit();
 	}
-	function enviarCola(){
-	    this.document.forms[0].hdAccion.value='enviarCola';    
-	    this.document.forms[0].action='ventaMedicamentosServlet';
-	    this.document.forms[0].submit();
+	function enviarCola() {
+		this.document.forms[0].hdAccion.value = 'enviarCola';
+		this.document.forms[0].action = 'ventaMedicamentosServlet';
+		this.document.forms[0].submit();
+	}
+
+	function carga() {
+		this.document.forms[0].hdAccion.value = 'carga';
+		this.document.forms[0].action = 'ventaMedicamentosServlet';
+		this.document.forms[0].submit();
 	}
 </script>
 
 </head>
 <body id="ventaMedicamentos">
-<form name="frmVentaMedicamentos">
+	<form name="frmVentaMedicamentos">
 
-	<!-- header -->
-	<div id="header">
-		<div class="container">
-			<!-- .logo -->
-			<div class="logo">
-				<a href="index.jsp"><img src="images/logo.jpg" alt="" /></a>
+		<!-- header -->
+		<div id="header">
+			<div class="container">
+				<!-- .logo -->
+				<div class="logo">
+					<a href="index.jsp"><img src="images/logo.jpg" alt="" /> </a>
+				</div>
+				<!-- /.logo -->
+				<!-- .nav -->
+				<ul class="nav">
+					<li><a href="frmRegistroReservaMedica.jsp">Reservas</a></li>
+					<li><a href="frmRegistroPagoConsulta.jsp">Pago Consulta</a></li>
+					<li><a href="frmRegistroConsultaMedica.jsp">Atención
+							Consulta</a></li>
+					<li><a href="frmVentaMedicamentos.jsp" class="current">Medicamentos</a>
+					</li>
+					<li><a href="frmRegistroExamenMedico.jsp">Examen Médico</a></li>
+				</ul>
+				<!-- /.nav -->
 			</div>
-			<!-- /.logo -->
-			<!-- .nav -->
-			<ul class="nav">
-				<li><a href="frmRegistroReservaMedica.jsp"  >Reservas</a></li>
-		      	<li><a href="frmRegistroPagoConsulta.jsp">Pago Consulta</a></li>        
-		        <li><a href="frmRegistroConsultaMedica.jsp">Atención Consulta</a></li>
-		        <li><a href="frmVentaMedicamentos.jsp" class="current">Medicamentos</a></li>
-		        <li><a href="frmRegistroExamenMedico.jsp" >Examen Médico</a></li>
-			</ul>
-			<!-- /.nav -->
 		</div>
-	</div>
 
-	<!-- content -->
-	<div id="content">
-		<div class="container">
-			<div class="inside">
-				<div class="wrapper">
-					<div class="mainContent">
-						<div class="section">
-							<!-- .box -->
-							<div class="extra-wrap">
-								<img height="50px" src="images/icon2.png" alt=""
-									class="png img-indent" />
-								<h2>Venta Medicamentos</h2>
-							</div>
-							<div class="box">
-								<div class="inner">
-									<h4>Datos Reserva Mèdica</h4>
-									<table width="100%">
-										<tr>
-											<td width="80px">Nro. Consulta:</td>
-											<td width="125px"><input type="text" size="15"  name="nroConsulta" value="${nroConsulta}">
-											</td>
-											<td width="30px">&nbsp;</td>
-											<td width><input type="button" class="button" 
-												value="Consultar"
-												onclick="javascript:obtenerDatosPaciente()">
-											</td>
-											<td width="60px">&nbsp;</td>
-										</tr>
-															
-										<tr>
-											<td width="80px" height="30px">Paciente:</td>
-											<td width="125px">${consulta.noPaciente}<input type="hidden" name="hdPaciente"></td>
-											<td width="30px">&nbsp;</td>
-											<td width="70px">D.N.I:</td>
-											<td width="95px">${consulta.dniPaciente}<input type="hidden" name="hdDni"></td>
-										</tr>
-									</table>
-									<br>
-									<h4>Resultado de Consulta Medica</h4>
-										<table border="1"  cellspacing="0" cellpadding="1">
+		<!-- content -->
+		<div id="content">
+			<div class="container">
+				<div class="inside">
+					<div class="wrapper">
+						<div class="mainContent">
+							<div class="section">
+								<!-- .box -->
+								<div class="extra-wrap">
+									<img height="50px" src="images/icon2.png" alt=""
+										class="png img-indent" />
+									<h2>Venta Medicamentos</h2>
+								</div>
+								<div class="box">
+									<div class="inner">
+										<h4>
+											Datos Reserva Mèdica<input type="button" class="button"
+												value="Carga de datos temporal" onclick="javascript:carga()">
+										</h4>
+										<table width="100%">
+											<tr>
+												<td width="80px">Nro. Consulta:</td>
+												<td width="125px"><input type="text" size="15"
+													name="nroConsulta">
+												</td>
+												<td width="30px">&nbsp;</td>
+												<td width><input type="button" class="button"
+													value="Consultar"
+													onclick="javascript:obtenerDatosPaciente()">
+												</td>
+												<td width="60px">&nbsp;</td>
+											</tr>
+
+											<tr>
+												<td width="80px" height="30px">Paciente:</td>
+												<td width="125px">${consulta.noPaciente}<input type="hidden" name="hdPaciente" ><input
+													type="hidden" name="hdPaciente"></td>
+												<td width="30px">&nbsp;</td>
+												<td width="70px">D.N.I:</td>
+												<td width="95px">${consulta.dniPaciente}<input type="hidden" name="hdDni" ><input
+													type="hidden" name="hdDni"></td>
+											</tr>
+										</table>
+										<br>
+										<h4>Resultado de Consulta Medica</h4>
+										<table border="1" cellspacing="0" cellpadding="1">
+											<tr>
+												<th width="70px">Codigo</th>
+
+												<th width="250px">Nombre Medicamento</th>
+
+												<th width="70px">Cantidad</th>
+
+												<th width="70px">Precio</th>
+
+											</tr>
+											<c:forEach var="medicina" items="${medicinas}">
 												<tr>
-													<th width="70px">Codigo</th>
-													
-													<th width="250px">Nombre Medicamento</th>
-													
-													<th width="70px">Cantidad</th>
-													
-													<th width="70px">Precio</th>
-													
+													<td><c:out value="${medicina.coMedicamento}" /></td>
+													<td><c:out value="${medicina.noMedicamento}" /></td>
+													<td><c:out value="${medicina.cantidad}" /></td>
+													<td><c:out value="${medicina.precio}" /></td>
 												</tr>
-					            <c:forEach var="medicina" items="${medicinas}">
-					                <tr>
-					                    <td><c:out value="${medicina.coMedicamento}"/></td>
-					                    <td><c:out value="${medicina.noMedicamento}"/></td>
-					                    <td><c:out value="${medicina.cantidad}"/></td>
-					                    <td><c:out value="${medicina.precio}"/></td>
-					                </tr>
-					            </c:forEach>
+											</c:forEach>
 
-											</table>
+										</table>
+									</div>
 								</div>
-							</div>
-							<!-- /.box -->
+								<!-- /.box -->
 
-							<!-- .box -->
-							<div class="box">
-								<div class="inner">
-									<h4>Datos del Seguro</h4>
+								<!-- .box -->
+								<div class="box">
+									<div class="inner">
+										<h4>Datos del Seguro</h4>
+										<table width="100%">
+											<tr>
+												<td width="240px">Asegurado: ${afililiado}</td>
+												<td width="240px">% Descuento: ${porcentajeDescuento}</td>
+
+											</tr>
+										</table>
+										<br>
+										<h4>Datos del Pago</h4>
+										<table width="100%">
+											<tr>
+
+											</tr>
+											<tr>
+												<td width="240px" height="20px">Monto a Pagar: ${monto}</td>
+												<td width="240px" height="20px">Descuento: ${descuento}<input
+													type="hidden" name="hdDescuento"></td>
+											</tr>
+											<tr>
+												<td width="240px" height="20px">Monto Total a Pagar:
+													${montoT}<input type="hidden" name="hdMontoT"></td>
+												<td width="240px">Monto Pagado: <input type="text"
+													size="8" name="efectivo">
+												</td>
+
+											</tr>
+											<tr>
+												<td width="240px" height="20px"></td>
+												<td width="240px" height="20px">Vuelto: ${vuelto}</td>
+											</tr>
+										</table>
+									</div>
+
 									<table width="100%">
 										<tr>
-											<td width="240px">Asegurado: ${afililiado}</td>
-											<td width="240px">% Descuento: ${porcentajeDescuento}</td>
-
-										</tr>
-									</table>
-									<br>
-									<h4>Datos del Pago</h4>
-									<table width="100%">
-										<tr>
-
-										</tr>
-										<tr>
-											<td width="240px" height="20px">Monto a Pagar: ${monto}</td>
-											<td width="240px" height="20px">Descuento: ${descuento}<input type="hidden" name="hdDescuento"></td>
-										</tr>
-										<tr>
-											<td width="240px" height="20px">Monto Total a Pagar: ${montoT}<input type="hidden" name="hdMontoT"></td>
-											<td width="240px">Monto Pagado: <input type="text" size="8" name="efectivo" >
+											<td align="center"><input type="button" class="button"
+												value="Registrar" onclick="javascript:enviarCola()">
 											</td>
+											<td align="center"><input
+												onclick="location.href='index.jsp'" type="button"
+												class="button" value="Cerrar"></td>
+											<td align="center">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+										</tr>
 
-										</tr>
-										<tr>
-											<td width="240px" height="20px"></td>
-											<td width="240px" height="20px">Vuelto: ${Vuelto} </td>
-										</tr>
+
 									</table>
+									<br> <br>
 								</div>
-
-								<table width="100%">
-									<tr>
-										<td align="center"><input type="button" class="button" 
-												value="Registrar"
-												onclick="javascript:enviarCola()"></td>
-										<td align="center"> <input onclick="location.href='index.jsp'" type="button" class="button" value="Cerrar"></td>
-										<td align="center">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-									</tr>
-
-
-								</table>
-								<br> <br>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- footer -->
-	<div id="footer">
-		<div class="container">
-			<div class="inside">&nbsp;&copy; 2011 &nbsp; &nbsp;<input type="hidden" name="hdAccion"></div>
+		<!-- footer -->
+		<div id="footer">
+			<div class="container">
+				<div class="inside">
+					&nbsp;&copy; 2011 &nbsp; &nbsp;<input type="hidden" name="hdAccion">
+				</div>
+			</div>
 		</div>
-	</div>
 	</form>
 </body>
 </html>
